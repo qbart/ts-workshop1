62f1985c07786e3876544a3c9f6279d43d0e502e
qbart <wojtowicz.bartlomiej@gmail.com>
2021-09-04 20:33:35 +0200

    answers 3

--- excersises/03_functions/functions.ts
+++ excersises/03_functions/functions.ts
@@ -1,3 +1,23 @@
-export function repeat() {}
+export type RepeatParams = {
+  times: number;
+  join: (false | string);
+};
 
-export function timeout(options) {}
+export function repeat(s: string, params: RepeatParams) {
+    let a = Array.from({length: params.times}).map(() => s);
+    if (params.join === false) {
+        return a;
+    }
+
+    return a.join(params.join);
+}
+
+export type Callback = (err: string|null, result: number) => void;
+export type TimeoutOptions = {
+    time: number;
+    callback: Callback;
+};
+
+export function timeout(o: TimeoutOptions) {
+  setTimeout(() => o.callback(null, o.time), o.time);
+}
